ab91e688ee2dc5720d70a9483a5d8755d47fb6b4
Phongsakon Konrad <124145979+pkyeager@users.noreply.github.com>
2023-11-25 12:21:10 +0100

    Merge branch 'erd' into Dev, merge conflicts

--- SustainACityMAUI/ViewModels/CrisisManagementMinigame.cs
+++ SustainACityMAUI/ViewModels/CrisisManagementMinigame.cs
@@ -1,123 +1,349 @@
+
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using SustainACityMAUI.Helpers;
 using SustainACityMAUI.Models;
 
 namespace SustainACityMAUI.ViewModels;
-
 public class CrisisManagementMinigame : BaseViewModel
 {
+
+    // Fields
     private readonly Player _player;
-    private List<DayDecision> _days;
+    private int _currentDay;
+    private Action _selectedAction;
+    private ObservableCollection<Action> _actions;
+    private int _days;
+    private Random random = new Random();
+    private System.Timers.Timer timer;
 
-    private readonly List<string> _decisions = new List<string>
-{
-    // Good decisions
-    "🏗️ Construct a hydroelectric dam",
-    "🏃‍♀️ Coordinate a city-wide evacuation",
-    "💰 Allocate additional funds to emergency services",
-    "🏢 Upgrade city infrastructure to withstand floods",
-    "👨‍🔬 Consult with environmental scientists",
-    "🎓 Launch a public education campaign about flood safety",
-    "💸 Organize a fundraising campaign for flood victims",
-    "🔬 Fund research into advanced flood prediction technology",
-    "👥 Establish a dedicated disaster response team",
-    "🏠 Promote the construction of flood-resistant buildings",
+    private bool _isStartButtonVisible = true;
 
-    // Neutral decisions
-    "🙏 Hold a city-wide day of prayer",
-    "🌤️ Invest in improved weather forecasting technology",
-    "🚰 Overhaul the city's drainage systems",
-    "💵 Encourage residents to purchase flood insurance",
-    "🔄 Develop a comprehensive flood recovery plan",
+    private bool _isListViewVisible = false;
+    private bool _isScoreLabelVisible = false;
 
-    // Bad decisions
-    "🙈 Ignore the flood warnings",
-    "🚨 Prematurely declare a state of emergency",
-    "🌊 Construct makeshift flood barriers",
-    "⚠️ Implement a basic flood warning system",
-    "🌱 Implement new soil management practices without research",
-};
+    private bool _isSubmitButtonVisible = true;
+    private int _temperature;
+    private int _rainfall;
+    private int _drainage;
 
-    public ICommand GoBackCommand { get; set; }
-    public ICommand SubmitDecisionsCommand { get; set; }
-    public ICommand SelectDecisionCommand { get; set; }
+    private int _waterLevel;
 
-    public List<DayDecision> Days
+    // Properties
+
+    public int CurrentDay
     {
-        get { return _days; }
+        get => _currentDay;
+        set
+        {
+            _currentDay = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Days
+    {
+        get => _days;
         set
         {
             _days = value;
-            OnPropertyChanged(nameof(Days));
+            OnPropertyChanged();
         }
     }
 
+    public int Temperature
+    {
+        get => _temperature;
+        set
+        {
+            _temperature = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Rainfall
+    {
+        get => _rainfall;
+        set
+        {
+            _rainfall = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Drainage
+    {
+        get => _drainage;
+        set
+        {
+            _drainage = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int WaterLevel
+    {
+        get => _waterLevel;
+        set
+        {
+            _waterLevel = value;
+            OnPropertyChanged();
+        }
+    }
+    public class Action
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+    }
+
+    public Action SelectedAction
+    {
+        get => _selectedAction;
+        set
+        {
+            _selectedAction = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsStartButtonVisible
+    {
+        get { return _isStartButtonVisible; }
+        set
+        {
+            _isStartButtonVisible = value;
+            OnPropertyChanged(nameof(IsStartButtonVisible));
+        }
+    }
+
+    public bool IsListViewVisible
+    {
+        get { return _isListViewVisible; }
+        set
+        {
+            _isListViewVisible = value;
+            OnPropertyChanged(nameof(IsListViewVisible));
+        }
+    }
+
+    public bool IsScoreLabelVisible
+    {
+        get { return _isScoreLabelVisible; }
+        set
+        {
+            _isScoreLabelVisible = value;
+            OnPropertyChanged(nameof(IsScoreLabelVisible));
+        }
+    }
+
+    public bool IsSubmitButtonVisible
+    {
+        get { return _isSubmitButtonVisible; }
+        set
+        {
+            _isSubmitButtonVisible = value;
+            OnPropertyChanged(nameof(IsSubmitButtonVisible));
+        }
+    }
+    public ICommand MoveUpCommand { get; }
+    public ICommand MoveDownCommand { get; }
+    public ICommand ShowDetailsCommand { get; }
+    public ICommand NextDayCommand { get; }
+    public ICommand SubmitCommand { get; private set; }
+    public ICommand StartGameCommand => new Command(() =>
+  {
+      IsStartButtonVisible = false;
+      IsListViewVisible = true;
+  });
+
+    [Obsolete]
+    public ICommand GoBackCommand => new Command(() =>
+    {
+        MessagingCenter.Send(this, "GoBack");
+    });
+
+    [Obsolete]
     public CrisisManagementMinigame(Player player)
     {
         _player = player;
-        GoBackCommand = new Command(GoBack);
-        SubmitDecisionsCommand = new Command(SubmitDecisions);
-        SelectDecisionCommand = new Command<string>(SelectDecision);
+        CurrentDay = 1;
+        Days = 7; // for a week-long game
+        Actions = new ObservableCollection<Action>
+    {
+        new Action { Id = 1, Name = "Plant Trees 🌳",Score= 8, Description = "Planting trees helps to reduce the water level by absorbing water. Trees take up water from the soil and release it into the atmosphere, a process known as transpiration. This not only helps to lower the water level but also contributes to the local climate and biodiversity." },
 
-        Days = new List<DayDecision>();
-        for (int i = 1; i <= 10; i++)
+        new Action { Id = 2, Name = "Build Dams 🏗️", Score = 10, Description = "Building dams can control the flow of water and prevent flooding. Dams store water and release it in a controlled manner, preventing sudden increases in water level downstream. They also provide a source of hydroelectric power, which is a clean and renewable source of energy." },
+
+        new Action { Id = 3, Name = "Improve Drainage 🚧", Score = 9, Description = "Improving the drainage system can effectively reduce the water level. A well-designed drainage system can quickly remove excess water from the surface, preventing waterlogging and flooding. It also helps to manage water resources more effectively and can improve the quality of local water bodies by preventing pollution." },
+
+        new Action { Id = 4, Name = "Promote Water Conservation 💧", Score = 7 , Description = "Promoting water conservation can reduce the demand for water and lower the water level. By encouraging people to use water more efficiently, we can reduce the amount of water that needs to be extracted from natural sources. This not only helps to lower the water level but also ensures a sustainable supply of water for future generations." },
+
+        new Action { Id = 5, Name = "Implement Rainwater Harvesting ☔", Score = 6 , Description = "Implementing rainwater harvesting can reduce the water level and provide a source of water. Rainwater harvesting systems collect and store rainwater for later use, reducing the demand for water from other sources. This not only helps to lower the water level but also provides a reliable and sustainable source of water, especially in areas with limited water resources." },
+
+        new Action { Id = 6, Name = "Restore Wetlands 🌾", Score = 8 ,Description = "Restoring wetlands can help to absorb excess water and reduce the water level. Wetlands act as natural sponges, absorbing and storing excess water and slowly releasing it over time. This not only helps to lower the water level but also improves local biodiversity and provides a habitat for many species." },
+
+        new Action { Id = 7, Name = "Upgrade Infrastructure 🏢", Score = 7 ,Description = "Upgrading infrastructure can improve water management and reduce the water level. This includes improving the design and construction of buildings and roads to make them more water-efficient, and upgrading water supply and sewage systems to reduce water loss and pollution." },
+
+        new Action { Id = 8, Name = "Ignore the Problem 😴",Score = -5, Description = "Ignoring the problem will lead to an increase in the water level. Without taking any action to manage the water level, it will continue to rise due to natural processes and human activities. This can lead to flooding and other water-related problems." },
+
+        new Action { Id = 9, Name = "Increase Water Usage 🚿", Score = -4, Description = "Increasing water usage will lead to an increase in the water level. More water usage means more water is extracted from natural sources, which can lead to a rise in the water level. It also puts more pressure on water resources, leading to potential water shortages in the future." },
+
+        new Action { Id = 10, Name = "Pollute the Water 💀", Score = -10, Description = "Polluting the water can make it unusable and increase the water level. Water pollution can come from various sources, including industrial waste, agricultural runoff, and domestic sewage. Polluted water is not only harmful to human health and the environment, but it also increases the water level as it cannot be used effectively." },
+
+        new Action { Id = 11, Name = "Monitor Situation 👀", Score = 2, Description = "Monitoring the situation doesn't directly affect the water level but keeps you informed. By keeping a close eye on the water level and other related factors, you can make informed decisions about what actions to take. This can help to prevent sudden increases in the water level and mitigate the impact of any potential problems." },
+
+        new Action { Id = 12, Name = "Research Solutions 📚", Score = 3, Description = "Researching solutions doesn't directly affect the water level but can lead to future actions. By studying the problem and exploring potential solutions, you can develop effective strategies for managing the water level. This can lead to more effective actions in the future, helping to keep the water level under control." }
+        // Add more actions as needed for now just 12 in total. Maybe I could add more later
+    };
+        // Initialize the timer with a 12-second interval
+        timer = new System.Timers.Timer(12000);
+
+        // Set the method to call when the timer elapses
+        timer.Elapsed += (sender, e) => GenerateData();
+
+        MoveUpCommand = new Command<Action>(MoveUp);
+        MoveDownCommand = new Command<Action>(MoveDown);
+        ShowDetailsCommand = new Command<Action>(ShowDetails);
+        SubmitCommand = new Command(Submit);
+
+    }
+
+    public void GenerateData()
+    {
+        // Generate random values for temperature, rainfall, and drainage system
+        // Increase the range of possible values based on the current day
+        Temperature = random.Next(1 + CurrentDay * 5, 40); // Temperature in degrees Celsius
+        Rainfall = random.Next(50, 100 + CurrentDay * 10); // Rainfall in mm, minimum is 50
+        Drainage = random.Next(0, 25 + CurrentDay * 10); // Drainage system capacity in %
+
+        // Calculate the water level based on these values
+        // This is a more complex calculation that considers more factors
+        double effectiveDrainage = Drainage * Math.Max(0, 1 - Rainfall / 200.0);
+        double evaporation = Temperature > 0 ? Temperature / 100.0 : 0;
+        int newWaterLevel = (int)(Rainfall - effectiveDrainage - evaporation * WaterLevel);
+
+        // Ensure the new water level is at least 1
+        newWaterLevel = Math.Max(newWaterLevel, 1);
+
+        // Add the new water level to the old one
+        WaterLevel += newWaterLevel;
+
+        // Ensure the water level doesn't go below zero
+        if (WaterLevel < 0)
         {
-            Days.Add(new DayDecision
-            {
-                Day = $"Day {i}",
-                Decisions = new ObservableCollection<string>(_decisions)
-            });
+            WaterLevel = 0;
+        }
+
+
+        // Notify the UI that the WaterLevel has changed
+        OnPropertyChanged(nameof(WaterLevel));
+    }
+
+    public ObservableCollection<Action> Actions
+    {
+        get => _actions;
+        set
+        {
+            _actions = value;
+            OnPropertyChanged();
         }
     }
-    private void GoBack()
+
+    public void StartGame()
     {
-        // Implement going back
+        // Start the timer
+        timer.Start();
+
+        // Generate initial data
+        GenerateData();
     }
-    private void SubmitDecisions()
+
+    public void StopGame()
     {
-        // Implement decision submission
+        // Stop the timer
+        timer.Stop();
     }
 
-    private void SelectDecision(string decision)
+
+    // Move the selected action up or down in the list
+    private void MoveUp(Action action)
     {
-        // Implement decision selection
-    }
-    public class DayDecision : BaseViewModel
-    {
-        private string _day;
-        private string _selectedDecision;
-        private ObservableCollection<string> _decisions;
-
-        public string Day
+        var index = Actions.IndexOf(action);
+        if (index > 0)
         {
-            get { return _day; }
-            set
-            {
-                _day = value;
-                OnPropertyChanged(nameof(Day));
-            }
-        }
-
-        public string SelectedDecision
-        {
-            get { return _selectedDecision; }
-            set
-            {
-                _selectedDecision = value;
-                OnPropertyChanged(nameof(SelectedDecision));
-            }
-        }
-
-        public ObservableCollection<string> Decisions
-        {
-            get { return _decisions; }
-            set
-            {
-                _decisions = value;
-                OnPropertyChanged(nameof(Decisions));
-            }
+            Actions.Move(index, index - 1);
         }
     }
+
+    private void MoveDown(Action action)
+    {
+        var index = Actions.IndexOf(action);
+        if (index < Actions.Count - 1)
+        {
+            Actions.Move(index, index + 1);
+        }
+    }
+
+    // Show the details of the selected action
+    private async void ShowDetails(Action action)
+    {
+        await Application.Current.MainPage.DisplayAlert(action.Name, action.Description, "Back to the game");
+    }
+
+
+    // Submit the selected actions and calculate the score
+    [Obsolete]
+    private async void Submit()
+    {
+        CalculateScore();
+        StopGame();
+        await Application.Current.MainPage.DisplayAlert("Score", $"Congrats! 🎉. Your score is {_player.Score}", "Go Back");
+        IsSubmitButtonVisible = false;
+        IsListViewVisible = false;
+        IsScoreLabelVisible = true;
+
+        // Execute the GoBackCommand
+        GoBackCommand.Execute(null);
+    }
+
+    // Calculate the score based on the current water level, time taken, and selected actions
+    public void CalculateScore()
+    {
+        int waterLevelFactor = 100 - _waterLevel; // Higher score for lower water level
+        int timeFactor = (int)timer.Interval; // Higher score for quicker times
+
+        int actionScore = 0;
+        for (int i = 0; i < _actions.Count; i++)
+        {
+            // Assume that the actions are sorted by rank, with the highest-ranked action first
+            // Give more points for higher-ranked actions
+            actionScore += (12 - i) * _actions[i].Score;
+        }
+
+        // Calculate the initial score
+        int initialScore = waterLevelFactor + timeFactor + actionScore;
+
+        // Calculate the percentage of the initial score
+        int minScore = 10800;
+        int maxScore = 13200;
+        double percentage = ((initialScore - minScore) * 100.0) / (maxScore - minScore);
+
+        // Ensure the percentage is within 0 to 100
+        percentage = Math.Max(0, Math.Min(100, percentage));
+
+        // Add the percentage to the existing score
+        _player.Score += (int)percentage;
+
+
+        // Score cap at 100
+        if (_player.Score > 100)
+        {
+            _player.Score = 100;
+        }
+    }
+
 
 }
+
--- SustainACityMAUI/Views/CrisisManagementMinigamePage.xaml.cs
+++ SustainACityMAUI/Views/CrisisManagementMinigamePage.xaml.cs
@@ -1,14 +1,57 @@
+using System.Diagnostics;
 using SustainACityMAUI.Models;
 using SustainACityMAUI.ViewModels;
 
-namespace SustainACityMAUI.Views;
-
-public partial class CrisisManagementMinigamePage : ContentPage
+namespace SustainACityMAUI.Views
 {
-    public CrisisManagementMinigamePage(Player player)
+    public partial class CrisisManagementMinigamePage : ContentPage
     {
-        InitializeComponent();
-        BindingContext = new CrisisManagementMinigame(player);
-    }
+        private Stopwatch _stopwatch;
+        private bool _gameStarted = false;
 
+        [Obsolete]
+        public CrisisManagementMinigamePage(Player player)
+        {
+            InitializeComponent();
+            BindingContext = new CrisisManagementMinigame(player);
+            _stopwatch = new Stopwatch();
+            StartGameButton.Clicked += OnStartGameButtonClicked;
+
+            // UI Styling
+            // Set the width of elements to be % of the screen width
+            ActionListView.WidthRequest = Application.Current.MainPage.Width * 0.90;
+            StartGameButton.WidthRequest = Application.Current.MainPage.Width * 0.90;
+            SubmitButton.WidthRequest = Application.Current.MainPage.Width * 0.90;
+
+            // Navigation
+            MessagingCenter.Subscribe<CrisisManagementMinigame>(this, "GoBack", (sender) =>
+   {
+       Navigation.PopAsync();
+
+   });
+        }
+
+        [Obsolete]
+        private void OnStartGameButtonClicked(object sender, EventArgs e)
+        {
+            if (!_gameStarted)
+            {
+                _gameStarted = true;
+                _stopwatch.Start();
+                Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+                {
+                    TimerLabel.Text = $"Time: {(int)_stopwatch.Elapsed.TotalSeconds} seconds";
+                    return true; // keeps the timer running
+                });
+                var game = (CrisisManagementMinigame)BindingContext;
+                game.StartGame();
+            }
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<CrisisManagementMinigame>(this, "GoBack");
+        }
+
+    }
 }
