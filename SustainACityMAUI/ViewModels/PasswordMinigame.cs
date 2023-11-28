cca955b0e6e4f6ef688058d292f180791faa7e23
Rikki Snaebjornsson <R.snaebjornsson@gmail.com>
2023-11-28 18:09:20 +0100

    updated ui and added fancy strings in PaswordMinigamePage

--- SustainACityMAUI/ViewModels/PasswordMinigame.cs
+++ SustainACityMAUI/ViewModels/PasswordMinigame.cs
@@ -1,4 +1,5 @@
 ﻿using SustainACityMAUI.Models;
+using SustainACityMAUI.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,4 +18,26 @@ public class PasswordMinigame : BaseViewModel
     {
         _player = player;
     }
+
+    public string backroundImg => "computer.jpeg";
+    public string[] OSstartup = { "UNLIM99 COMPUTER BY LUCANUS PORCIUS", "DEBUG ROM V3.0, CARD: NO NAME", "BYTES FREE: 1284096", "Hello Daniel! It sure feels good to be out of that tiny memory card. Bye!", "BREAK AT 0117", "READY" };
+    public string instructions => "adddddddddddddddddddddsd";
+    public string instructionss()
+    {
+        string s = "aasdas";
+        return s;
+    }
+
+
+
+    public void initConsole()
+    {
+        for (int i = 0; i < OSstartup.Length; i++)
+        {
+            Label label = new Label { Text = OSstartup[i] };
+            label.FontAttributes = FontAttributes.Bold;
+            
+        }
+    }
+
 }
